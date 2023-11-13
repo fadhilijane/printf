e3c1c504e7f3b15d183a7e1c137bc16a99601665
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-11-13 21:54:07 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -94,13 +94,13 @@ int _printf(const char *format, ...)
 					counter += _putdigit((long)va_arg(args, unsigned int), 10);
 					break;
 				case 'o':
-					counter += _putdigit((long)va_arg(args, int), 8);
+					counter += _putdigit((long)va_arg(args, unsigned int), 8);
 					break;
 				case 'x':
-					counter += _putdigit((long)va_arg(args, unsigned int), 16); 
+					counter += _putdigit((long)va_arg(args, int), 16); 
 				   	break;
 				case 'X':
-					counter += _putdigit((long)va_arg(args, unsigned int), 16);
+					counter += _putdigit((long)va_arg(args, int), 16);
 					break;
 				default:
 					counter += write(1, format, 1);
