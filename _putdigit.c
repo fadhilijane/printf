435d47a705a1fb660eb27350a5a2d635ce67f854
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-11-13 21:57:54 +0300

    Create _putdigit.c

---
+++ _putdigit.c
@@ -0,0 +1,28 @@
+/**
+ * _putdigit - The function to print a number in a specified base.
+ * @num: Parameter representing the number to be printed.
+ * @base: Parameter representing the base to print the number into.
+ * Return: +1 if num is negative
+ */
+
+int _putdigit(long num, int base)
+{
+	int count_digit;
+	char *numbers;
+
+	numbers = "0123456789abcdef";
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		return (_putdigit(-num, base) + 1);
+	}
+	else if (num < base)
+	{
+		return (_putchar(numbers[num]));
+	}
+	else
+	{
+		count_digit = _putdigit(num / base, base);
+		return (count_digit + _putdigit(num % base, base));
+	}
+}
